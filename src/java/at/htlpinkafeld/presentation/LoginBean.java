dd1b764be91139dcfd48023499f6be37d20e95ca
fuchsber <Bernif8@gmail.com>
2017-11-03 19:00:16 +0100

    LoginBean + login.xhtml hinzugefügt

---
+++ src/java/at/htlpinkafeld/presentation/LoginBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.htlpinkafeld.presentation;
+
+/**
+ *
+ * @author Bernhard
+ */
+public class LoginBean {
+    public String username;
+    public String password;
+    
+    public LoginBean() {
+    }
+
+    public LoginBean(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public Object doLogin()
+    {
+        return null;
+    }
+}
